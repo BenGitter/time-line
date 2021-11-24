2d6f2c968e3edc8bb7d77756ac4f71a85a299bac
Benjamin van Zwienen <benjaminvanzwienen@live.nl>
2021-11-24 13:07:17 +0100

    Add accurate date ticks in all zoom modes

--- main.js
+++ main.js
@@ -41,7 +41,11 @@ const zoom_handler = () => {
     no_future();
     update_svg_pos();
     draw_endpoints();
-    draw_blocks();
+    update_zoom_mode();
+    const [t, y_t] = calculate_ticks()
+    draw_ticks(t);
+    draw_year_ticks(y_t);
+    // draw_blocks();
     
     svg.attr('transform', `translate(${svg_transform.x}, ${svg_transform.y}) scale(${svg_transform.k}, 1)`)
 };
@@ -102,6 +106,159 @@ Current approach does not work since months (and years) are not of equal length.
     a. create label with correct date format (depending on zoom_mode).
     b. create block from prev to next tick.
 */
+
+// Date::getTime() returns number of *milliseconds* since Jan 1, 1970
+const DAY_MILLIS = 24*60*60*1000;
+const WEEK_MILLIS = 7*24*60*60*1000;
+const MONTH_MILLIS = 31*24*60*60*1000; // for 31-day month
+const YEAR_MILLIS = 365*24*60*60*1000; // for 365-day year
+
+const max_time_window = {
+    'day': 16*DAY_MILLIS,
+    'week': 9*WEEK_MILLIS,
+    'month': 24*MONTH_MILLIS,
+    'year': 20*YEAR_MILLIS,
+};
+
+const next_zoom_mode = {
+    'day': zoom_modes.WEEK,
+    'week': zoom_modes.MONTH,
+    'month': zoom_modes.YEAR,
+    'year': zoom_modes.YEAR,
+}
+const prev_zoom_mode = {
+    'day': zoom_modes.DAY,
+    'week': zoom_modes.DAY,
+    'month': zoom_modes.WEEK,
+    'year': zoom_modes.MONTH,
+}
+
+const update_zoom_mode = () => {
+    const start_date = time_scale.invert(svg_pos.start);
+    const end_date = time_scale.invert(svg_pos.end);
+    const time_window = end_date.getTime() - start_date.getTime();
+
+    // update zoom_mode if time_window is too big or too small
+    if (time_window > max_time_window[zoom_mode]) {
+        zoom_mode = next_zoom_mode[zoom_mode];
+    } else if (time_window < max_time_window[prev_zoom_mode[zoom_mode]]) {
+        zoom_mode = prev_zoom_mode[zoom_mode];
+    }
+};
+
+const calculate_ticks = () => {
+    const start_date = time_scale.invert(svg_pos.start);
+    const end_date = time_scale.invert(svg_pos.end);
+    end_date.setHours(0,0,0,0);
+    start_date.setHours(0,0,0,0);
+
+    let ticks = [];
+    let year_ticks = [];
+
+    if (start_date.getFullYear() != end_date.getFullYear() && zoom_mode != zoom_modes.YEAR) {
+        const first_year = new Date(start_date.getFullYear()+1, 0, 1);
+        const last_year = new Date(end_date.getFullYear(), 0, 1);
+        year_ticks.push({ x: time_scale(first_year), d: first_year.getFullYear() });
+        if (first_year < last_year) year_ticks.push({ x: time_scale(last_year), d: last_year.getFullYear() });
+    }
+
+    if (zoom_mode == zoom_modes.DAY) {
+        start_date.setDate(start_date.getDate()+1);
+        end_date.setDate(end_date.getDate());
+        const num_ticks = (end_date.getTime() - start_date.getTime()) / DAY_MILLIS +1;
+
+        for (let i = 0; i < num_ticks; i++) {
+            let d = new Date(start_date.getTime());
+            d.setDate(d.getDate() + i);
+            ticks.push({ x: time_scale(d), d: d.toDateString().substring(4, 10) });
+        }
+    } else if (zoom_mode == zoom_modes.WEEK) {
+        start_date.setDate(start_date.getDate() - start_date.getDay() +7);
+        end_date.setDate(end_date.getDate() - end_date.getDay());
+        const num_ticks = (end_date.getTime() - start_date.getTime()) / WEEK_MILLIS +1;
+
+        for (let i = 0; i < num_ticks; i++) {
+            let d = new Date(start_date.getTime());
+            d.setDate(d.getDate() + 7*i);
+            ticks.push({ x: time_scale(d), d: d.toDateString().substring(4, 10) });
+        }
+    } else if (zoom_mode == zoom_modes.MONTH) {
+        start_date.setDate(1);
+        start_date.setMonth(start_date.getMonth()+1);
+        end_date.setDate(1);
+        const num_ticks = Math.round((end_date.getTime() - start_date.getTime()) / MONTH_MILLIS) +1;
+
+        for (let i = 0; i < num_ticks; i++) {
+            let d = new Date(start_date.getTime());
+            d.setMonth(d.getMonth() + i);
+            let s = d.toDateString().substring(4, 7) + d.toDateString().substring(10) 
+            ticks.push({ x: time_scale(d), d: d.toDateString().substring(4, 7) });
+        }
+    } else if (zoom_mode == zoom_modes.YEAR) {
+        start_date.setDate(1);
+        start_date.setMonth(0);
+        start_date.setFullYear(start_date.getFullYear()+1);
+        end_date.setDate(1);
+        end_date.setMonth(0);
+        const num_ticks = Math.round((end_date.getTime() - start_date.getTime()) / YEAR_MILLIS) +1;
+
+        for (let i = 0; i < num_ticks; i++) {
+            let d = new Date(start_date.getTime());
+            d.setFullYear(d.getFullYear() + i);
+            ticks.push({ x: time_scale(d), d: d.getFullYear() });
+        }
+    }
+
+    if (ticks.length > 25) {
+        console.log('[WARNING] Too many ticks');
+    }
+    return [ticks, year_ticks];
+};
+
+const draw_ticks = (ticks) => {
+    svg.selectAll('rect.tick').remove();
+    svg.selectAll('text.tick').remove();
+    for (const tick of ticks) {
+        svg
+            .append('rect')
+            .attr('class', 'tick')
+            .attr('x', tick.x-2 / svg_transform.k)
+            .attr('y', OPTIONS.center_y-5)
+            .attr('width', 2 / svg_transform.k)
+            .attr('height', 20)
+            .style('fill', 'red');
+        svg
+            .append('text')
+            .attr('class', 'tick')
+            .text(tick.d)
+            .attr('x', (tick.x-0) * svg_transform.k)
+            .attr('y', OPTIONS.center_y-5)
+            .attr('transform', `scale(${1/svg_transform.k},1)`)
+    }
+}
+
+const draw_year_ticks = (ticks) => {
+    svg.selectAll('rect.year_tick').remove();
+    svg.selectAll('text.year_tick').remove();
+    for (const tick of ticks) {
+        svg
+            .append('rect')
+            .attr('class', 'year_tick')
+            .attr('x', tick.x-2 / svg_transform.k)
+            .attr('y', OPTIONS.center_y-35)
+            .attr('width', 2 / svg_transform.k)
+            .attr('height', 40)
+            .style('fill', 'red');
+        svg
+            .append('text')
+            .attr('class', 'year_tick')
+            .text(tick.d)
+            .attr('x', (tick.x) * svg_transform.k +3)
+            .attr('y', OPTIONS.center_y-20)
+            .attr('transform', `scale(${1/svg_transform.k},1)`)
+    }
+}
+
 const draw_blocks = () => {
     const block_width = OPTIONS.block_base_width * block_width_multiplyer[zoom_mode];
     let first_block_num = Math.ceil(Math.abs(svg_pos.end) / block_width);
@@ -119,45 +276,6 @@ const draw_blocks = () => {
         num_blocks++;
     }
 
-    const prev_zoom_mode = zoom_mode;
-    switch (zoom_mode) {
-        case zoom_modes.DAY:
-            if (num_blocks > 21) {
-                // console.log('switch day', num_blocks)
-                zoom_mode = zoom_modes.WEEK;
-            }
-            break;
-        case zoom_modes.WEEK:
-            if (num_blocks > 9) {
-                // console.log('switch week', num_blocks)
-                zoom_mode = zoom_modes.MONTH;
-            } else if (num_blocks < 4) {
-                // console.log('switch week', num_blocks)
-                zoom_mode = zoom_modes.DAY;
-            }
-            break;
-        case zoom_modes.MONTH:
-            if (num_blocks > 24) {
-                // console.log('switch month', num_blocks)
-                zoom_mode = zoom_modes.YEAR;
-            } else if (num_blocks < 3) {
-                // console.log('switch month', num_blocks)
-                zoom_mode = zoom_modes.WEEK;
-            }
-            break;
-        case zoom_modes.YEAR:
-            if (num_blocks > 20) {
-                // console.log('switch year', num_blocks)
-            } else if (num_blocks < 3) {
-                // console.log('switch week', num_blocks)
-                zoom_mode = zoom_modes.MONTH;
-            }
-            break;
-    }
-    
-    if (prev_zoom_mode != zoom_mode)
-        return draw_blocks();
-
     svg.selectAll('rect.block').remove();
     svg.selectAll('text.block').remove();
 
@@ -172,13 +290,13 @@ const draw_blocks = () => {
             .attr('width', block_width)
             .attr('height', 10)
             .style('fill', colors[zoom_mode][i%2]);
-        svg
-            .append('text')
-            .attr('class', 'block')
-            .text(d.toDateString().substring(4, 10))
-            .attr('x', (x-0) * svg_transform.k)
-            .attr('y', OPTIONS.center_y-5)
-            .attr('transform', `scale(${1/svg_transform.k},1)`)
+        // svg
+        //     .append('text')
+        //     .attr('class', 'block')
+        //     .text(d.toDateString().substring(4, 10))
+        //     .attr('x', (x-0) * svg_transform.k)
+        //     .attr('y', OPTIONS.center_y-5)
+        //     .attr('transform', `scale(${1/svg_transform.k},1)`)
     }
 
 };
@@ -191,6 +309,9 @@ const no_future = () => {
 };
 
 draw_endpoints()
+const [t, y_t] = calculate_ticks()
+draw_ticks(t);
+draw_year_ticks(y_t);
 draw_blocks()
 /*
 TODO:
